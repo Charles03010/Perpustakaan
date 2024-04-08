fab666096889a2d75b868c0a7ed38e7c03a490a5
Charles03010 <71315698+Charles03010@users.noreply.github.com>
2024-04-08 23:53:14 +0700

    feat(dashboard-and-pengarang): view dashboard and pengarang

--- resources/views/dashboard-admin.blade.php
+++ resources/views/dashboard-admin.blade.php
@@ -1,68 +1,181 @@
 @include('partials.header', ['title' => 'Dashboard Admin'])
 @include('partials.scripts')
+
 <body>
 
-  <!-- ======= Header ======= -->
-  @include('components.header')
+    <!-- ======= Header ======= -->
+    @include('components.header')
 
-  @include('partials.sidebar', ['active' => "dashboard"])
+    @include('partials.sidebar', ['active' => 'dashboard'])
 
-  <main id="main" class="main">
+    <main id="main" class="main">
 
-    <div class="pagetitle">
-      <h1>Blank Page</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item">Pages</li>
-          <li class="breadcrumb-item active">Blank</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
+        <div class="pagetitle">
+            <h1>Dashboard</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('dashboard-admin') }}">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
+            </nav>
+        </div><!-- End Page Title -->
 
-    <section class="section">
-      <div class="row">
-        <div class="col-lg-6">
+        <section class="section dashboard">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <!-- Sales Card -->
+                        <div class="col-xxl-3 col-md-6">
+                            <div class="card info-card sales-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Peminjaman <span>| Today</span></h5>
 
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Example Card</h5>
-              <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-bookmark"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>145</h6>
+                                            <span class="text-success small pt-1 fw-bold">12%</span> <span
+                                                class="text-muted small pt-2 ps-1">increase</span>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div><!-- End Sales Card -->
+                        <!-- Sales Card -->
+                        <div class="col-xxl-3 col-md-6">
+                            <div class="card info-card sales-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Buku <span>| Today</span></h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-book"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>145</h6>
+                                            <span class="text-success small pt-1 fw-bold">12%</span> <span
+                                                class="text-muted small pt-2 ps-1">increase</span>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div><!-- End Sales Card -->
+                        <!-- Sales Card -->
+                        <div class="col-xxl-3 col-md-6">
+                            <div class="card info-card sales-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Skripsi <span>| Today</span></h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-blockquote-left"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>145</h6>
+                                            <span class="text-success small pt-1 fw-bold">12%</span> <span
+                                                class="text-muted small pt-2 ps-1">increase</span>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div><!-- End Sales Card -->
+                        <!-- Sales Card -->
+                        <div class="col-xxl-3 col-md-6">
+                            <div class="card info-card sales-card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Pengguna <span>| Today</span></h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-person-circle"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>145</h6>
+                                            <span class="text-success small pt-1 fw-bold">12%</span> <span
+                                                class="text-muted small pt-2 ps-1">increase</span>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div><!-- End Sales Card -->
+                        <div class="col-xxl-12 col-md-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Riwayat Peminjaman</h5>
+
+                                    <!-- Table with hoverable rows -->
+                                    <table class="table table-hover table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col" colspan="2">No.</th>
+                                                <th scope="col">Nama</th>
+                                                <th scope="col">Judul</th>
+                                                <th class="max-content" scope="col">Tanggal Pinjam</th>
+                                                <th class="max-content" scope="col">Tanggal Kembali</th>
+                                                <th class="max-content" scope="col">Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr class="align-middle">
+                                                <td class="max-content" scope="row">1</td>
+                                                <td class="max-content">
+                                                  <a href="" class="w-3 p-2 rounded bg-danger me-2">
+                                                    <i class="bi bi-trash text-white"></i>
+                                                  </a>
+                                                  <a href="" class="w-3 p-2 rounded bg-warning">
+                                                    <i class="bi bi-pencil text-white"></i>
+                                                  </a>
+                                                </td>
+                                                <td class="max-content">Brandon Jacob</td>
+                                                <td>Designer</td>
+                                                <td>2016-05-25</td>
+                                                <td>2016-05-25</td>
+                                                <td><span class="badge rounded-pill bg-warning">Dipinjam</span></td>
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                    <!-- End Table with hoverable rows -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
+        </section>
 
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+        <div class="copyright">
+            &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
         </div>
-
-        <div class="col-lg-6">
-
-          <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">Example Card</h5>
-              <p>This is an examle page with no contrnt. You can use it as a starter for your custom pages.</p>
-            </div>
-          </div>
-
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
         </div>
-      </div>
-    </section>
+    </footer><!-- End Footer -->
 
-  </main><!-- End #main -->
-
-  <!-- ======= Footer ======= -->
-  <footer id="footer" class="footer">
-    <div class="copyright">
-      &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
-    </div>
-    <div class="credits">
-      <!-- All the links in the footer should remain intact. -->
-      <!-- You can delete the links only if you purchased the pro version. -->
-      <!-- Licensing information: https://bootstrapmade.com/license/ -->
-      <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-      Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-    </div>
-  </footer><!-- End Footer -->
-
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
 </body>
 
 </html>
---
+++ resources/views/pengarang-admin.blade.php
@@ -0,0 +1,94 @@
+@include('partials.header', ['title' => 'Dashboard Admin'])
+@include('partials.scripts')
+
+<body>
+
+    <!-- ======= Header ======= -->
+    @include('components.header')
+
+    @include('partials.sidebar', ['active' => 'dashboard'])
+
+    <main id="main" class="main">
+
+        <div class="pagetitle">
+            <h1>Pengarang</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('dashboard-admin') }}">Home</a></li>
+                    <li class="breadcrumb-item active">Pengarang</li>
+                </ol>
+            </nav>
+        </div><!-- End Page Title -->
+
+        <section class="section dashboard">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-xxl-12 col-md-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Pengarang</h5>
+                                    <a href="" class="btn btn-primary">
+                                        <i class="bi bi-plus-square"></i>
+                                        Tambah Data
+                                    </a>
+                                    <!-- Table with hoverable rows -->
+                                    <table class="table table-hover table-striped">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col" colspan="2">No.</th>
+                                                <th scope="col">Nama</th>
+                                                <th scope="col">Email</th>
+                                                <th scope="col">No HP</th>
+                                                <th scope="col">Alamat</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr class="align-middle">
+                                                <td class="max-content" scope="row">1</td>
+                                                <td class="max-content">
+                                                  <a href="" class="w-3 p-2 rounded bg-danger me-2">
+                                                    <i class="bi bi-trash text-white"></i>
+                                                  </a>
+                                                  <a href="" class="w-3 p-2 rounded bg-warning">
+                                                    <i class="bi bi-pencil text-white"></i>
+                                                  </a>
+                                                </td>
+                                                <td>Brandon Jacob</td>
+                                                <td>Brandon@gmail.com</td>
+                                                <td>081389905680</td>
+                                                <td>Kampus 1 UTY Jombor</td>
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                    <!-- End Table with hoverable rows -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main><!-- End #main -->
+
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+        <div class="copyright">
+            &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            <!-- All the links in the footer should remain intact. -->
+            <!-- You can delete the links only if you purchased the pro version. -->
+            <!-- Licensing information: https://bootstrapmade.com/license/ -->
+            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+</body>
+
+</html>
